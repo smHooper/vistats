c275337b1444aa497042a8f127c99bbbcfffc664
smhooper <samhooperstudio@gmail.com>
2020-08-07 17:33:45 -0800

    run scripts on different days

--- py/run_monthly_scripts.py
+++ py/run_monthly_scripts.py
@@ -15,9 +15,9 @@ def main():
     now = datetime.now()
 
     if now.day == 5:
-
         retrieve_data.main(RETRIEVE_DATA_PARAMS)
 
+    if now.day == 7:
         count_datetime = (now - relativedelta.relativedelta(months=1)).replace(day=1)
         send_notifications.main(SEND_NOTIFICATIONS_PARAMS, count_datetime.strftime('%Y-%m-%d'))
 
